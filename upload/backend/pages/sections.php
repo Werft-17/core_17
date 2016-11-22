fc33e4d2fef9e94a9f2f890b20c4fa55ba4b3b7c
aldus <drp@cms-lab.com>
2016-11-22 22:57:28 +0100

    Bugfixes for sections
    remove also unused code

--- upload/backend/pages/sections.php
+++ upload/backend/pages/sections.php
@@ -72,122 +72,142 @@ if(!isset($_GET['page_id']) OR !is_numeric($_GET['page_id']))
 require_once(LEPTON_PATH.'/framework/class.admin.php');
 $admin = new admin('Pages', 'pages_modify');
 
-
-//	// Check if we are supposed to add or delete a section
-//	if(isset($_GET['section_id']) AND is_numeric($_GET['section_id']))
-//	{
-//		// Get more information about this section
-//		$section_id = intval($_GET['section_id']);
-//	    $sql  = 'SELECT `module` FROM `'.TABLE_PREFIX.'sections` ';
-//	    $sql .= 'WHERE `section_id` ='.$section_id;
-//	    $query_section = $database->query($sql);
-//	
-//		if($query_section->numRows() == 0)
-//	    {
-//			$admin->print_error('Section not found');
-//		}
-//		$section = $query_section->fetchRow( MYSQL_ASSOC );
-//		// Include the modules delete file if it exists
-//		if(file_exists(LEPTON_PATH.'/modules/'.$section['module'].'/delete.php'))
-//	    {
-//			require(LEPTON_PATH.'/modules/'.$section['module'].'/delete.php');
-//		}
-//	    $sql  = 'DELETE FROM `'.TABLE_PREFIX.'sections` ';
-//	    $sql .= 'WHERE `section_id` ='.$section_id.' LIMIT 1';
-//	    $query_section = $database->query($sql);
-//	
-//		if($database->is_error())
-//	    {
-//			$admin->print_error($database->get_error());
-//		} else {
-//			require(LEPTON_PATH.'/framework/class.order.php');
-//			$order = new order(TABLE_PREFIX.'sections', 'position', 'section_id', 'page_id');
-//			$order->clean($page_id);
-//			$admin->print_success($TEXT['SUCCESS'], ADMIN_URL.'/pages/sections.php?page_id='.$page_id);
-//			$admin->print_footer();
-//			exit();
-//		}
-//	} elseif(isset($_POST['module']) && $_POST['module'] != '')
-//	{
-
-if( (isset($_POST['job'])) && ($_POST['job'] == "add") )
+/**
+ *	Any "jobs" to do?
+ */
+if( isset($_POST['job']) )
 {
-	if ( (isset($_POST['module'])) && ($_POST['module'] != ''))
+	$job = trim($_POST['job']);
+	
+	if( $job == "delete")
 	{
-
-		// Get module name
-		/**
-		 *	MOTICE!
-		 *	LEPTON 2.3.µ+ and newer: we are submitting the "ADDON_ID" instead of the Name!
-		 */
-		$module = preg_replace("/\W/", "", addslashes($_POST['module']));  // fix secunia 2010-91-4
-
-		/**
-		 *	Is the module-name valide? Or in other words: does the module(-name) exists?
-		 *
-		 */
-		$temp_result = array();
-		$database->execute_query(
-			"SELECT `name` from `".TABLE_PREFIX."addons` where `addon_id`='".$module."'",
-			true,
-			$temp_result,
-			false
-		);
-		if (true===$database->is_error())
+		if ( (isset($_POST['section_id'])) && ($_POST['section_id'] != ''))
 		{
-			$admin->print_error($database->get_error());
-		}
-		else 
-		{
-			if ( 0 === count($temp_result) )
+
+			$section_id = intval($_POST['section_id']);
+		
+			$section_info = array();
+			$database->execute_query(
+				"SELECT `module` FROM `".TABLE_PREFIX."sections` WHERE `section_id` =".$section_id,
+				true,
+				$section_info,
+				false
+			);
+		
+			if( 0 === count($section_info) )
 			{
-				$admin->print_error($MESSAGE['GENERIC_MODULE_VERSION_ERROR']." [1]");
+				$admin->print_error('Section not found');
+			}
+		
+			/**
+			 *	Call "delete.php" of the module
+			 */
+			$look_for_path = LEPTON_PATH.'/modules/'.$section_info['module'].'/delete.php';
+		 
+			if(file_exists($look_for_path))
+			{
+				global $page_id, $section_id; // aldus: it is not realy clear if we need this for the delete procedere of the modules!
+			
+				require( $look_for_path );
+			}
+		
+			$database->simple_query("DELETE FROM `".TABLE_PREFIX.'sections` WHERE `section_id` ='.$section_id);
+			if($database->is_error())
+			{
+				$admin->print_error($database->get_error());
+			} else {
+				require(LEPTON_PATH.'/framework/class.order.php');
+				$order = new order(TABLE_PREFIX.'sections', 'position', 'section_id', 'page_id');
+				$order->clean($page_id);
+				$admin->print_success($TEXT['SUCCESS']."\nDelete section ".$section_id, ADMIN_URL.'/pages/sections.php?page_id='.$page_id);
+				$admin->print_footer();
+				exit();
 			}
 		}
-		$module = $temp_result['directory'];
-		unset($temp_result);	
-		echo LEPTON_tools::display( $module );
-		/**
-		 *	Got the current user the rights to "use" this module at all?
-		 *
-		 */
-		if (true === in_array($module, $_SESSION['MODULE_PERMISSIONS'] ) )
-		{
-			$admin->print_error($MESSAGE['GENERIC_NOT_UPGRADED']);
-		}
-		
-		
-		// Include the ordering class
-		require(LEPTON_PATH.'/framework/class.order.php');
-		// Get new order
-		$order = new order(TABLE_PREFIX.'sections', 'position', 'section_id', 'page_id');
-		$position = $order->get_new($page_id);	
-	
-		// Insert 'module' into DB
-	    $fields = array(
-	    	'page_id'	=> $page_id,
-	    	'module'	=> $module,
-	    	'position'	=> $position,
-	    	'block'		=> 1			// Attention: insert a new module-section here at block 1
-	    );
-
-	    $database->build_and_execute(
-	    	'insert',
-	    	TABLE_PREFIX.'sections',
-	    	$fields
-	    );
-
-		// Get the section id
-		$section_id = $database->get_one("SELECT LAST_INSERT_ID()");	
-		// Include the selected modules add file if it exists
-		if(file_exists(LEPTON_PATH.'/modules/'.$module.'/add.php'))
-	    {
-			require(LEPTON_PATH.'/modules/'.$module.'/add.php');
-		}	
 	}
-}
+	//		End: delete section
+	
+	if( $job == "add" )
+	{
+		if ( (isset($_POST['module'])) && ($_POST['module'] != ''))
+		{
 
-//	END: insert a new section
+			// Get module name
+			/**
+			 *	MOTICE!
+			 *	LEPTON 2.3.µ+ and newer: we are submitting the "ADDON_ID" instead of the Name!
+			 */
+			$module = preg_replace("/\W/", "", addslashes($_POST['module']));  // fix secunia 2010-91-4
+
+			/**
+			 *	Is the module-id valide? Or in other words: does the module(-name) exists?
+			 *
+			 */
+			$temp_result = array();
+			$database->execute_query(
+				"SELECT `name`, `directory` from `".TABLE_PREFIX."addons` where `addon_id`='".$module."'",
+				true,
+				$temp_result,
+				false
+			);
+			if (true===$database->is_error())
+			{
+				$admin->print_error($database->get_error());
+			}
+			else 
+			{
+				if ( 0 === count($temp_result) )
+				{
+					$admin->print_error($MESSAGE['GENERIC_MODULE_VERSION_ERROR']." [1]");
+				}
+			}
+			
+			// echo (LEPTON_tools::display($temp_result));
+			
+			$module = $temp_result['directory'];
+			unset($temp_result);	
+			echo LEPTON_tools::display( $module );
+
+			/**
+			 *	Got the current user the rights to "use" this module?
+			 *
+			 */
+			if (true === in_array($module, $_SESSION['MODULE_PERMISSIONS'] ) )
+			{
+				$admin->print_error($MESSAGE['GENERIC_NOT_UPGRADED']);
+			}
+
+			// Include the ordering class
+			require(LEPTON_PATH.'/framework/class.order.php');
+			// Get new order
+			$order = new order(TABLE_PREFIX.'sections', 'position', 'section_id', 'page_id');
+			$position = $order->get_new($page_id);	
+	
+			// Insert 'module' into DB
+			$fields = array(
+				'page_id'	=> $page_id,
+				'module'	=> $module,
+				'position'	=> $position,
+				'block'		=> 1			// Attention: insert a new module-section here at block 1
+			);
+
+			$database->build_and_execute(
+				'insert',
+				TABLE_PREFIX.'sections',
+				$fields
+			);
+
+			// Get the section id
+			$section_id = $database->get_one("SELECT LAST_INSERT_ID()");	
+			// Include the selected modules add file if it exists
+			if(file_exists(LEPTON_PATH.'/modules/'.$module.'/add.php'))
+			{
+				require(LEPTON_PATH.'/modules/'.$module.'/add.php');
+			}	
+		}
+	}
+	//	END: insert a new section
+}
 
 /**
  *	Get page details
@@ -311,251 +331,6 @@ echo $parser->render(
 	$page_vars
 );
 
-#if($query_sections->numRows() > 0)
-#{
-#	$num_sections = $query_sections->numRows();
-#	while($section = $query_sections->fetchRow( MYSQL_ASSOC ))
-//		foreach($all_sections as $section)
-//	    {
-//			if(!is_numeric(array_search($section['module'], $module_permissions)))
-//	        {
-//				// Get the modules real name
-//	            $sql = 'SELECT `name` FROM `'.TABLE_PREFIX.'addons` ';
-//	            $sql .= 'WHERE `directory` = "'.$section['module'].'"';
-//	            if(!$database->get_one($sql) || !file_exists(LEPTON_PATH.'/modules/'.$section['module']))
-//				{
-//					$edit_page = '<span class="module_disabled">'.$section['module'].'</span>';
-//				} 
-//				else
-//				{
-//					$edit_page = '';
-//				}
-//				$edit_page_0 = '<a id="sid'.$section['section_id'].'" href="'.ADMIN_URL.'/pages/modify.php?page_id='.$page_id;
-//				$edit_page_1 = $section['section_id'].'">'.$section['module'].'</a>';
-//				if(SECTION_BLOCKS)
-//	            {
-//					if($edit_page == '')
-//					{
-//						$edit_page = $edit_page_0.'#'.SEC_ANCHOR.$edit_page_1;
-//					}
-//					$input_attribute = 'input_normal';
-//					$template->set_var(array(
-//						'STYLE_DISPLAY_SECTION_BLOCK' => ' style="visibility:visible;"',
-//						'NAME_SIZE' => 300,
-//						'INPUT_ATTRIBUTE' => $input_attribute,
-//						'VAR_SECTION_ID' => $section['section_id'],
-//						'VAR_POSITION' => $section['position'],
-//						'LINK_MODIFY_URL_VAR_MODUL_NAME' => $edit_page,
-//						'SELECT' => '',
-//						'SET_NONE_DISPLAY_OPTION' => ''
-//						)
-//					);
-//					// Add block options to the section_list
-//					$template->clear_var('block_list');
-//					foreach($block as $number => $name)
-//	                {
-//						$template->set_var('NAME', htmlentities(strip_tags($name)));
-//						$template->set_var('VALUE', $number);
-//						$template->set_var('SIZE', 1);
-//						if($section['block'] == $number)
-//	                    {
-//							$template->set_var('SELECTED', ' selected="selected"');
-//						} else {
-//							$template->set_var('SELECTED', '');
-//						}
-//						$template->parse('block_list', 'block_block', true);
-//					}
-//				} else {
-//					if($edit_page == '')
-//					{
-//						$edit_page = $edit_page_0.'#'.SEC_ANCHOR.$edit_page_1;
-//					}
-//					$input_attribute = 'input_normal';
-//					$template->set_var(array(
-//						'STYLE_DISPLAY_SECTION_BLOCK' => ' style="visibility:hidden;"',
-//						'NAME_SIZE' => 300,
-//						'INPUT_ATTRIBUTE' => $input_attribute,
-//						'VAR_SECTION_ID' => $section['section_id'],
-//						'VAR_POSITION' => $section['position'],
-//						'LINK_MODIFY_URL_VAR_MODUL_NAME' => $edit_page,
-//						'NAME' => htmlentities(strip_tags($block[1])),
-//						'VALUE' => 1,
-//						'SET_NONE_DISPLAY_OPTION' => ''
-//						)
-//					);
-//				}
-//				
-//				$template->set_var("SECTION_NAME", htmlentities($section['name']) );
-//				
-//				// Insert icon and images
-//				$template->set_var(array(
-//					'CLOCK_16_PNG' => 'calendar_add_16.png',
-//					'CLOCK_DEL_16_PNG' => 'calendar_del_16.png',
-//					'DELETE_16_PNG' => 'delete_16.png'
-//					) 
-//				);
-//				
-//				/**
-//				 *	set calendar start value
-//				 */
-//				$template->set_var('VALUE_PUBL_START', ( ($section['publ_start'] == 0) ? '' : date($jscal_format, $section['publ_start']) ) );
-//				
-//				/**
-//				 *	set calendar end value
-//				 */
-//				$template->set_var('VALUE_PUBL_END', ( ($section['publ_end'] == 0) ? '' :  date($jscal_format, $section['publ_end']) ) );
-//				
-//				/**
-//				 *	Insert icons for moving the section up or down
-//				 */
-//				$template->set_var(
-//					'VAR_MOVE_UP_URL',
-//					( ( $section['position'] > 1 )
-//					? '<a href="'.ADMIN_URL.'/pages/move_up.php?page_id='.$page_id.'&amp;section_id='.$section['section_id'].'"><img src="'.THEME_URL.'/images/up_16.png" alt="{TEXT_MOVE_UP}" /></a>'
-//					: '' )
-//				);
-//				
-//				$template->set_var(
-//					'VAR_MOVE_DOWN_URL',
-//					( ($section['position'] < $num_sections )
-//					? '<a href="'.ADMIN_URL.'/pages/move_down.php?page_id='.$page_id.'&amp;section_id='.$section['section_id'].'"><img src="'.THEME_URL.'/images/down_16.png" alt="{TEXT_MOVE_DOWN}" /></a>'
-//					: '' )
-//				);
-//				
-//			} else {
-//			  continue; // m.f.i.
-//			}
-//			
-//			// m.f.i. - obsolete
-//			$template->set_var(array(
-//				'DISPLAY_DEBUG' => '',
-//				'TEXT_SID' => 'SID',
-//				'DEBUG_COLSPAN_SIZE' => 9
-//				) 
-//			);
-//			
-//			$template->parse('section_list', 'section_block', true);
-//		}
-//	#}
-//	
-//	// now add the calendars -- remember to to set the range to [1970, 2037] if the date is used as timestamp!
-//	// the loop is simply a copy from above.
-//	$sql  = 'SELECT `section_id`,`module` FROM `'.TABLE_PREFIX.'sections` ';
-//	$sql .= 'WHERE page_id = '.$page_id.' ';
-//	$sql .= 'ORDER BY `position` ASC';
-//	$query_sections = $database->query($sql);
-//	
-//	$num_sections = $query_sections->numRows();
-//	if( $num_sections > 0 )
-//	{
-//		$temp_calendar_show_time = ( ( isset($jscal_use_time) ) && ( $jscal_use_time==TRUE ) ) ? "true" : "false";
-//		
-//		while( false !== ($section = $query_sections->fetchRow( MYSQL_ASSOC ) ) )
-//	    {
-//			// Get the modules real name
-//	        $sql  = 'SELECT `name` FROM `'.TABLE_PREFIX.'addons` ';
-//	        $sql .= 'WHERE `directory` = "'.$section['module'].'"';
-//	        $module_name = $database->get_one($sql, MYSQL_ASSOC );
-//	
-//			if(!is_numeric(array_search($section['module'], $module_permissions)))
-//	        {
-//				$template->set_var(array(
-//					'VAR_SECTION_ID'	=> $section['section_id'],
-//					'jscal_ifformat'	=> $jscal_ifformat,
-//					'jscal_firstday'	=> $jscal_firstday,
-//					'jscal_today'		=> $jscal_today,
-//					'start_date'		=> 'start_date'.$section['section_id'],
-//					'end_date'			=> 'end_date'.$section['section_id'],
-//					'trigger_start'		=> 'trigger_start'.$section['section_id'],
-//					'trigger_end'		=> 'trigger_stop'.$section['section_id'],
-//					'showsTime'			=> $temp_calendar_show_time,
-//					'timeFormat'		=> "24"
-//					) 
-//				);
-//			}
-//			
-//			$template->parse('calendar_list', 'calendar_block', true);
-//		}
-//		
-//		unset($temp_calendar_show_time);
-//		
-//		$template->parse("calendar_link", "calendar_block_link", true);
-//	
-//	} else {
-//		$template->parse('calendar_list', '' );
-//	}
-//	
-//	// Work-out if we should show the "Add Section" form
-//	$sql  = 'SELECT `section_id` FROM `'.TABLE_PREFIX.'sections` ';
-//	$sql .= 'WHERE `page_id` = '.$page_id.' AND `module` = "menu_link"';
-//	$query_sections = $database->query($sql);
-//	if($query_sections->numRows() == 0)
-//	{
-//		// Modules list
-//	    $sql  = 'SELECT `name`,`directory`,`type` FROM `'.TABLE_PREFIX.'addons` ';
-//	    $sql .= 'WHERE `type` = "module" AND `function` = "page" AND `directory` != "menu_link" ';
-//	    $sql .= 'ORDER BY `name`';
-//	    $result = $database->query($sql);
-//	
-//		if( true === (0 < $result->numRows() ) )
-//	    {
-//			while( false !== ($module = $result->fetchRow( MYSQL_ASSOC ) ) )
-//	        {
-//				/**
-//				 *	Check if user is allowed to use this module
-//				 */
-//				if(!is_numeric(array_search($module['directory'], $module_permissions)))
-//	            {
-//					$template->set_var('VALUE', $module['directory']);
-//					$template->set_var('NAME', $module['name']);
-//					$template->set_var('SELECTED', ($module['directory'] == 'wysiwyg') ? ' selected="selected"' : '' );
-//	
-//					$template->parse('module_list', 'module_block', true);
-//				}
-//			}
-//		}
-//	}
-//	
-//	/**
-//	 *	Insert language text and messages
-//	 */
-//	$template->set_var(array(
-//		'TEXT_MANAGE_SECTIONS'	=> $HEADING['MANAGE_SECTIONS'],
-//		'TEXT_ARE_YOU_SURE'		=> $TEXT['ARE_YOU_SURE'],
-//		'TEXT_TYPE'				=> $TEXT['TYPE'],
-//		'TEXT_ADD'				=> $TEXT['ADD'],
-//		'TEXT_SAVE'				=> $TEXT['SAVE'],
-//		'TEXTLINK_MODIFY_PAGE'	=> $HEADING['MODIFY_PAGE'],
-//		'TEXT_CALENDAR'			=> $TEXT['CALENDAR'],
-//		'TEXT_DELETE_DATE'		=> $TEXT['DELETE_DATE'],
-//		'TEXT_ADD_SECTION'		=> $TEXT['ADD_SECTION'],
-//		'TEXT_MOVE_UP'			=> $TEXT['MOVE_UP'],
-//		'TEXT_MOVE_DOWN'		=> $TEXT['MOVE_DOWN']
-//		)
-//	);
-//	
-//	// Get display name of person who last modified the page
-//	$user=$admin->get_user_details($results_array['modified_by']);
-//	
-//	$modified_ts = ($results_array['modified_when'] != 0)
-//	        ? $modified_ts = date(TIME_FORMAT.', '.DATE_FORMAT, $results_array['modified_when'])
-//	        : 'Unknown';
-//	
-//	$template->set_var(array(
-//				'MODIFIED_BY' => $user['display_name'],
-//				'MODIFIED_BY_USERNAME' => $user['username'],
-//				'MODIFIED_WHEN' => $modified_ts,
-//				'LAST_MODIFIED' => $MESSAGE['PAGES_LAST_MODIFIED'],
-//		)
-//	);
-//	$template->parse('main', 'main_block', false);
-//	$template->pparse('output', 'page');
-//	
-//	// include the required file for Javascript admin
-//	if(file_exists(LEPTON_PATH.'/modules/jsadmin/jsadmin_backend_include.php'))
-//	{
-//		include(LEPTON_PATH.'/modules/jsadmin/jsadmin_backend_include.php');
-//	}
 
 // Print admin footer
 $admin->print_footer();
