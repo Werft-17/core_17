24d282cb84def7d4d37d88737a7c6d9c9038061f
aldus <drp@cms-lab.com>
2017-01-18 16:37:29 +0100

    Minor changes inside lib_twig

--- upload/modules/lib_twig/classes/lib_twig.php
+++ upload/modules/lib_twig/classes/lib_twig.php
@@ -5,53 +5,51 @@
  * This ADDON is released under the GNU GPL.
  * Additional license terms can be seen in the info.php of this module.
  *
- * @module          Twig Template Engine
- * @author          LEPTON Project
- * @copyright       2012-2017 LEPTON  
- * @link            https://www.LEPTON-cms.org
- * @license         http://www.gnu.org/licenses/gpl.html
- * @license_terms   please see info.php of this module
+ * @module			Twig Template Engine
+ * @author			LEPTON Project
+ * @copyright		2012-2017 LEPTON  
+ * @link			https://www.LEPTON-cms.org
+ * @license			http://www.gnu.org/licenses/gpl.html
+ * @license_terms	please see info.php of this module
  *
  */
 
 class lib_twig
 {
 	/**
-     * Registers Twig_Autoloader as an SPL autoloader.
-     *
-     * @param bool $prepend Whether to prepend the autoloader or not.
-     */
-    public static function register($prepend = true)
-    {
+	 * Registers Twig_Autoloader as an SPL autoloader.
+	 *
+	 * @param bool $prepend Whether to prepend the autoloader or not.
+	 */
+	public static function register($prepend = false)
+	{
 		spl_autoload_register(array(__CLASS__, 'autoload'), true, $prepend);
-    }
+	}
 
 	/**
-     * Handles autoloading of classes.
-     *
-     * @param string $class A class name.
-     */
-    public static function autoload($class)
-    {
-         
-        if (0 !== strpos($class, 'Twig'))
-        {
-            return;
-        }
+	 * Handles autoloading of classes.
+	 *
+	 * @param string $class A class name.
+	 */
+	public static function autoload($class)
+	{
+		if (0 !== strpos($class, 'Twig'))
+		{
+			return;
+		}
 
-        $lookup = str_replace(
-        	array('_', "\0"),
-        	array('/', ''),
-        	$class
-        );
+		$lookup = str_replace(
+			array('_', "\0"),
+			array('/', ''),
+			$class
+		);
 
-        $file = dirname(dirname(__FILE__)).'/'.$lookup.'.php';
-        
-        if (is_file($file))
-        {
-            require $file;
-        }
-    }
+		$file = dirname(dirname(__FILE__)).'/'.$lookup.'.php';
+
+		if (is_file($file))
+		{
+			require $file;
+		}
+	}
 }
-
 ?>
