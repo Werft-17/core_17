906d642a1bf183328a2c3fea8e6016339b91f03c
labby <rp@cms-lab.com>
2017-03-15 10:45:30 +0100

    add new marker

--- upload/templates/lepsem/languages/EN.php
+++ upload/templates/lepsem/languages/EN.php
@@ -34,6 +34,7 @@ $template_description 	= 'Enhanced backend theme for LEPTON CMS.';
 
 $THEME = array(
 	'ADMIN_ONLY' 		=> 'Settings for administrator only',
+	'ALLOWED_FILES' 	=> 'Allowed files',		
 	'NO_SHOW_THUMBS' 	=> 'Hide thumbnails',
 	'TEXT_HEADER' 		=> 'Set maximum imagesize for a folder</b><br><small><i>(resizing on new uploads only)</i></small>',
 	'ADDON_PERMISSIONS' => 'Addon Permissions',	
