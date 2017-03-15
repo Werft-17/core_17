f23b3e30e8c60950f5499ef018458f79de6faca5
aldus <drp@cms-lab.com>
2017-03-15 10:30:57 +0100

    Temp bugfix for Backend

--- upload/templates/lepsem/backend_body.js
+++ upload/templates/lepsem/backend_body.js
@@ -67,42 +67,4 @@ $('.message .close')
   $(function() {
     $( "#datepicker" ).datepicker();
   });
-
   
- /* enable drag and drop plugin (external)     
-$(document).ready(function() {
-    // Initialise the table
-    $("#page_table1,#page_table2").tableDnD();
-});
-*/
-$(document).ready(function() {
-
-	// Initialise the first table (as before)
-	//$("#lepsem_page_tree").tableDnD();
-
-	// Make a nice striped effect on the table
-	// $("#table-2 tr:even').addClass('alt')");
-
-	// Initialise the second table specifying a dragClass and an onDrop function that will display an alert
-	$("#table-3").tableDnD({
-	    onDragClass: "myDragClass",
-	    onDrop: function(table, row) {
-            var rows = table.tBodies[0].rows;
-            var debugStr = "Row dropped was "+row.id+". New order: ";
-            for (var i=0; i<rows.length; i++) {
-                debugStr += rows[i].id+" ";
-            }
-	        $(#debugArea).html(debugStr);
-	    },
-		onDragStart: function(table, row) {
-			$(#debugArea).html("Started dragging row "+row.id);
-		}
-	});
-	
-	$("#table-3 tr").hover(function() {
-          $(this.cells[0]).addClass('showDragHandle');
-    }, function() {
-          $(this.cells[0]).removeClass('showDragHandle');
-    });
-});
-
